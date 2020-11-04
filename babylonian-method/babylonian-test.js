7493113db102544b279ed68f41d1c2a44a473b3a
Shafee <shafeelinks@gmail.com>
2020-11-03 21:09:18 -0500

    added fixes to test files, and recurrent

--- babylonian-method/babylonian-test.js
+++ babylonian-method/babylonian-test.js
@@ -1,68 +1,68 @@
-const squareRoot = require('./answer')
-const test = require('tape')
+const squareRoot = require('./babylonian.js');
+const test = require('tape');
 
 test('it calculates an integer square root', assert => {
-  const radicand = 16
-  const actual = squareRoot(radicand)
-  const expected = 4
-  assert.equal(actual, expected, 'should get 4 as the square root of 16')
+  const radicand = 16;
+  const actual = squareRoot(radicand);
+  const expected = 4;
+  assert.equal(actual, expected, 'should get 4 as the square root of 16');
 
-  assert.end()
-})
+  assert.end();
+});
 
 test('it calculates a decimal square root', assert => {
-  const radicand = 423432.342423
-  const actual = squareRoot(radicand)
-  const expected = 650.7167912563806
+  const radicand = 423432.342423;
+  const actual = squareRoot(radicand);
+  const expected = 650.7167912563806;
 
-  assert.equal(actual, expected, 'should ')
+  assert.equal(actual, expected, 'should ');
 
-  assert.end()
-})
+  assert.end();
+});
 
 test('it produces synonymous results with JS native Math.sqrt fn', assert => {
-  const radicand = 423432.342423
-  const actual = squareRoot(radicand)
-  const expected = Math.sqrt(radicand)
+  const radicand = 423432.342423;
+  const actual = squareRoot(radicand);
+  const expected = Math.sqrt(radicand);
 
-  assert.equal(actual, expected, 'should be the same result as Math.sqrt')
+  assert.equal(actual, expected, 'should be the same result as Math.sqrt');
 
-  assert.end()
-})
+  assert.end();
+});
 
 test('it calculates infinite fraction square roots', assert => {
-  const radicand = 10
-  const actual = squareRoot(radicand)
-  const expected = 3.162277660168379
+  const radicand = 10;
+  const actual = squareRoot(radicand);
+  const expected = 3.162277660168379;
 
-  assert.equal(actual, expected, 'should properly end infinite decimal square roots')
+  assert.equal(actual, expected, 'should properly end infinite decimal square roots');
 
-  assert.end()
-})
+  assert.end();
+});
 
 test('it returns NaN if a negative value is passed as the radicand', assert => {
-  const negativeRadicand = -10
-  const actual = squareRoot(negativeRadicand)
+  const negativeRadicand = -10;
+  const actual = squareRoot(negativeRadicand);
 
-  assert.ok(isNaN(actual), 'should return NaN for negative radicand')
+  assert.ok(isNaN(actual), 'should return NaN for negative radicand');
 
-  assert.end()
-})
+  assert.end();
+});
 
 test('it returns NaN if a non-number is passed as the radicand', assert => {
-  const stringRadicand = 'some string'
-  const actual = squareRoot(stringRadicand)
+  const stringRadicand = 'some string';
+  const actual = squareRoot(stringRadicand);
 
-  assert.ok(isNaN(actual), 'should return NaN for non-number radicand')
+  assert.ok(isNaN(actual), 'should return NaN for non-number radicand');
 
-  assert.end()
-})
+  assert.end();
+});
 
 test('it returns NaN if NaN is passed as the radicand', assert => {
-  const nanRadicand = NaN
-  const actual = squareRoot(nanRadicand)
+  const nanRadicand = NaN;
+  const actual = squareRoot(nanRadicand);
 
-  assert.ok(isNaN(actual), 'should return NaN for NaN radicand')
+  assert.ok(isNaN(actual), 'should return NaN for NaN radicand');
 
-  assert.end()
-})
+  assert.end();
+});
--- number-frequency/most-recurrent.js
+++ number-frequency/most-recurrent.js
@@ -2,8 +2,9 @@
 
 
 const mostRecurrent = (set) => {
-
-
+  return set.sort((a, b) =>
+    set.filter(v => v === a).length - set.filter(v => v === b).length
+  ).pop();
 };
 
 
