10a56defdc5a4fd5b86510570c8d76cdc1c2cd6b
Shafee <shafeelinks@gmail.com>
2020-11-12 01:19:51 -0500

    balanced parens and add eventing

--- balanced-parens/balanced-parens.js
+++ balanced-parens/balanced-parens.js
@@ -1,5 +1,33 @@
-const parensAreBalanced = (input) => {
-  return false
-}
+const parensAreBalanced = input => {
 
-module.exports = parensAreBalanced
+  const openers = ['(', '{', '['];
+  const closers = [')', '}', ']'];
+
+  let temp = [];
+
+  for (item of input.split('')) {
+    if (openers.includes(item)) {
+      temp.push(item);
+    }
+  }
+
+  for (item of input.split('')) {
+    if (closers.includes(item)) {
+      temp.push(item);
+    }
+  }
+
+
+  if (temp.length % 2 === 1) {
+    return false;
+  }
+
+  return true;
+
+};
+
+
+console.log(parensAreBalanced('([])'));
+
+
+module.exports = parensAreBalanced;
