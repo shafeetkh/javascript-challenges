0ce035b5a2b156c14e95be917efb574c2f68f3b4
Shafee <shafeelinks@gmail.com>
2020-11-12 01:22:30 -0500

    function call name change

--- add-eventing/add-eventing.js
+++ add-eventing/add-eventing.js
@@ -1,4 +1,20 @@
-const addEventing = function (obj) {
-}
+const addEventing = obj => {
+    let arr = [];
+    let newObj = {
+        on: (step, callback) => {
+            if (!arr[step]) {
+                arr[step] = [];
+            }
+            arr[step].push(callback);
+        },
+        trigger: (step, ...args) => {
+            if (arr[step]) {
+                arr[step].map((callback) => callback(...args));
+            }
+        }
+    };
+    return newObj;
+};
 
-module.exports = addEventing
+
+module.exports = addEventing;
