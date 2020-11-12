396daac74b5b387f0f4b23ec00a7745a4a796975
Shafee <shafeelinks@gmail.com>
2020-11-12 02:52:33 -0500

    stack machine solution passed tests

--- stack-machine-calculator/stack-machine-calculator.js
+++ stack-machine-calculator/stack-machine-calculator.js
@@ -1,5 +1,60 @@
-const stackMachineCalculator = (instructions) => {
-  return instructions
-}
+const stackMachineCalculator = instructions => {
 
-module.exports = stackMachineCalculator
+  let stackArr = [];
+
+
+  if (instructions.includes(' ')) {
+    stackArr.unshift(instructions.split(' '));
+  }
+
+  let flatArr = stackArr.flat();
+  let temp = flatArr.filter(item => !isNaN(Number(item)));
+
+
+
+  for (item of flatArr) {
+    switch (true) {
+      case (flatArr.includes('+')):
+        return temp.reduce((acc, val) => {
+          return (Number(acc) + Number(val));
+        }, 0);
+        break;
+      case (flatArr.includes('-')):
+        return temp.reduce((acc, val) => {
+          return (Number(val) - Number(acc));
+        });
+        break;
+      case (flatArr.includes('*')):
+        return temp.reduce((acc, val) => {
+          return (Number(acc) * Number(val));
+        }, 1);
+        break;
+      case (flatArr.includes('/')):
+        return temp.reduce((acc, val) => {
+          return (Number(acc) / Number(val));
+        });
+        break;
+      case (flatArr.includes('POP')):
+        flatArr.pop();
+        console.log(flatArr);
+        break;
+      case (flatArr.includes('DUP')):
+        let topVal = flatArr[0];
+        flatArr.unshift(topVal), flatArr.pop();
+        console.log(flatArr);
+        break;
+    }
+  }
+
+  if (instructions.split(' ').length === 1) {
+    return Number(instructions.split(' ')[0]);
+  } else {
+    return Number(flatArr.pop());
+  }
+
+
+};
+
+console.log(stackMachineCalculator("13"));
+
+module.exports = stackMachineCalculator;
